8bef556c02d2a8d507778f25856ca10774acb918
bropav <pasha.puzach1@gmail.com>
2024-10-28 22:01:11 +0300

    new script

--- Do yyou like this game/Assets/scripts/start.cs
+++ Do yyou like this game/Assets/scripts/start.cs
@@ -7,7 +7,7 @@ public class start : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("лохи ебаные");
+        Debug.Log("yes");
     }
 
     // Update is called once per frame
